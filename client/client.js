eb0eebde10ac57d7274012b120d6a7811f383266
Jared Greene <jegreene@gmail.com>
2016-08-21 17:47:49 -0400

    Shell for Store Search with Redux React and Webpack

--- client/client.js
+++ client/client.js
@@ -1,1 +1,30 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App'
+import configureStore from '../redux/store'
+import { Provider } from 'react-redux'
 
+//let is for scope of course
+let initialState = {
+	todos: [{
+		id: 0,
+		completed: false,
+		text: 'Initial todo for demo'
+	}],
+	user: {
+		username: 'jared',
+		id: 13
+	}
+}
+
+let store = configureStore(initialState)
+
+//configure and create our store
+//createStore (reducers, initital state)
+
+render(
+  <Provider store={store}>
+  	<App/>
+  </Provider>,
+  document.getElementById('app')
+)
